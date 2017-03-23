9bccd187ba5e3a33f1aa518b890da4410e988cda
ronenpi18 <ronenpi18@gmail.com>
2017-03-23 22:45:05 +0200

    first commit

---
+++ loginComp/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://admin:TuKie3HQjoB05R3k@SG-test-9630.servers.mongodirector.com:27017/admin' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
